6022ce58f713b4cdf112a7ea4d745e4a92b65d75
MisterRoy <donald.damoue.etu@esmt.sn>
2022-01-20 21:29:28 +0000

    update readme.md

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://192.168.1.21:4000/api';
+const apiUrl = 'http://192.168.1.4:4000/api';
 
 export async function getProductTypes() {
   const response = await axios.get(`${apiUrl}/productType`);
